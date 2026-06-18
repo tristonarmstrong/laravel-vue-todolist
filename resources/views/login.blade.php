25e04e71b9fb0b1f104df4e7d92dfc7e1f07d36e
Triston Armstrong <tarmstrong95@proton.me>
2026-06-18 09:40:52 -0600

    remove light theme gross

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,7 @@
         @vite(['resources/js/app.js', 'resources/css/app.css'])
 
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <div class="flex flex-col gap-4 container max-w-3xs">
             <h1 class="text-lg font-bold text-white/50 mx-auto">Go ahead and authenticate</h1>
 
